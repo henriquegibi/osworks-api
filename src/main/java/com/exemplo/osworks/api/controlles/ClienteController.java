74a6562f5b47cf99bcc53dc931a8e87b64ec7eba
Henrique Gibi <henriquedepadua@yahoo.com.br>
2020-11-15 10:43:45 -0300

    Criando POST na aplicação

--- src/main/java/com/exemplo/osworks/api/controlles/ClienteController.java
+++ src/main/java/com/exemplo/osworks/api/controlles/ClienteController.java
@@ -8,27 +8,33 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.exemplo.osworks.domain.model.Cliente;
 import com.exemplo.osworks.domain.repository.ClienteRepository;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController
 {
 	@Autowired
 	private ClienteRepository clienteRepository;
 	
-	@GetMapping("/clientes")
+	@GetMapping
 	public List<Cliente> listar()
 	{
 		return clienteRepository.findAll();
 	}
 	
-	@GetMapping("/cliente/{clienteId}")
+	@GetMapping("/{clienteId}")
 	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId)
 	{
 		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
@@ -39,5 +45,12 @@ public class ClienteController
 		}
 		
 		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@RequestBody Cliente cliente)
+	{
+		return clienteRepository.save(cliente);
 	}
 }
