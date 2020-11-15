a50a8517d14b73469526fe13c3caf4e947bf65cd
Henrique Gibi <henriquedepadua@yahoo.com.br>
2020-11-15 10:23:31 -0300

    Corrigindo resposta código HTTP

--- src/main/java/com/exemplo/osworks/api/controlles/ClienteController.java
+++ src/main/java/com/exemplo/osworks/api/controlles/ClienteController.java
@@ -2,12 +2,15 @@ package com.exemplo.osworks.api.controlles;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.exemplo.osworks.domain.model.Cliente;
@@ -23,5 +26,18 @@ public class ClienteController
 	public List<Cliente> listar()
 	{
 		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/cliente/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId)
+	{
+		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+		
+		if(cliente.isPresent())
+		{
+			return ResponseEntity.ok(cliente.get());
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 }
